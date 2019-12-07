09fc7e511a9d4912e68a5c45bbb979091f53915a
afshinamighi <afshinamighi@gmail.com>
2019-12-07 21:48:45 +0100

    Delegates example is updated.

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace Delegates
 {
@@ -11,11 +12,17 @@ namespace Delegates
             int t1 = 5;
             int res1 = 0;
             res1 = fun1(t1);
+            Thread thread1 = new Thread(() => fun1(t1));
+            thread1.Start();
+            thread1.Join();
             Console.WriteLine("[Test 1] result is {0} , local variable is {1}, function returns {2} ", res1, t1, fun1(t1));
 
             int t2 = 5;
             int res2 = 0;
             Func<int> fun2 = () => { t2++; return t2; };
+            Thread thread2 = new Thread(() => fun2());
+            thread2.Start();
+            thread2.Join();
             res2 = fun2();
             Console.WriteLine("[Test 2] result is {0} , local variable is {1}, function returns {2} ", res2, t2, fun2());
 
